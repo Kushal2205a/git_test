9641d313dfd9b056906d7c48f83273221f81768d
Kushal2205a <introvertedspacemine2@gmail.com>
2025-05-31 18:21:22 +0530

    added contents and wrote some beginner stuff

---
+++ git_learnn.py
@@ -0,0 +1,10 @@
+#A repo contains a hidden git directory where git stores all of its internal tracking and versioning info
+"""
+untracked : Not being tracked by git 
+staged : Marked for inclusion in the next commit 
+committed: Saved to repository's history
+
+the git status command shows the current status of the repo
+"""
+
+
